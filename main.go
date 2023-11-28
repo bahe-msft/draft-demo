cb975bd74f8917868d1d6fecf9bbaaa62082959e
hbc <bcxxxxxx@gmail.com>
2023-11-28 14:14:30 -0800

    feat: create http server

--- main.go
+++ main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("hello, world")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	http.ListenAndServe("127.0.0.1:8888", nil)
 }
