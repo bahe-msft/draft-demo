149131af0e6b60f0ccce724304651f3e2160f787
hbc <bcxxxxxx@gmail.com>
2023-12-04 15:59:12 -0800

    chore: listen to 0.0.0.0

--- main.go
+++ main.go
@@ -1,14 +1,25 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log/slog"
 	"net/http"
 )
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		slog.InfoContext(r.Context(), "handling incoming request")
+		defer func() {
+		slog.InfoContext(r.Context(), "handled incoming request")
+		}()
+
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
-	http.ListenAndServe("127.0.0.1:8888", nil)
+	slog.InfoContext(context.TODO(), "starting server")
+	if err := http.ListenAndServe(":8888", nil); err != nil {
+		slog.ErrorContext(context.TODO(), "failed to start server", slog.String("error", err.Error()))
+		panic(err)
+	}
 }
